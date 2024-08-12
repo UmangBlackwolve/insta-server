24a4e6bbe2926cd421aaa5c3190430488c117f10
Ajay <Ajay>
2024-08-12 17:36:39 +0530

    edit to reguler mode in mongo connection

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
-const mongoose = require('mongoose')
-const { MONGOURI } = require('./config/keys')
-const cors = require('cors')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
-app.use(cors())
-mongoose.connect(MONGOURI)
-
-mongoose.connection.on('connected', () => {
-  console.log('connected to mongoDB Yah')
+app.use(cors());
+require('dotenv').config();
+mongoose.connect(process.env.MONGOURI)
+.then(() => {
+  console.log('Connected to MongoDB');
 })
+.catch(err => {
+  console.error('Error connecting to MongoDB', err);
+});
 
-mongoose.connection.on('error', (err) => {
-  console.log('err',err)
-})
+require('./models/user');
+require('./models/post');
 
-require('./models/user')
-require('./models/post')
+app.use(express.json());
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
+app.use(require('./routes/user'));
 
 
-app.use(express.json())
-app.use(require('./routes/auth'))
-app.use(require('./routes/post'))
-app.use(require('./routes/user'))
-
-if(process.env.NODE_ENV=="production"){
-  app.use(express.static('client/build'))
-  const path = require('path')
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
