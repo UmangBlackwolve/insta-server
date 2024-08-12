759b64f9f50365a6728b5252a7103a628411ec55
Ajay <Ajay>
2024-08-12 18:16:43 +0530

    .

--- routes/post.js
+++ routes/post.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const requireLogin = require('../middleware/requireLogin')
 const Post = mongoose.model('Post')
 
-router.get('/allposts', requireLogin, (req, res) => {
+router.get('/allposts', (req, res) => {
   Post.find()
     .populate("postedBy", "_id name")
     .populate("comments.postedBy", "_id name")
